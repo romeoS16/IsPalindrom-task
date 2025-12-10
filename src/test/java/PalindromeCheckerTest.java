0f6d8330502c1a6dbdd3c626e72f35d77f79e775
romeoS16 <romeoschweizer29@gmail.com>
2025-12-10 15:17:58 +0100

    weitere tests

--- src/test/java/PalindromeCheckerTest.java
+++ src/test/java/PalindromeCheckerTest.java
@@ -7,6 +7,27 @@ import static org.junit.Assert.assertTrue;
 
 public class PalindromeCheckerTest {
 
+    // A: Spezialfälle
+
+    @Test
+    public void nullInput_returnsFalse() {
+        assertFalse(PalindromeChecker.isPalindrome(null));
+    }
+
+    @Test
+    public void emptyString_returnsTrue() {
+        assertTrue(PalindromeChecker.isPalindrome(""));
+    }
+
+    @Test
+    public void onlySpaces_returnsTrue() {
+        // Nach Entfernen der Leerzeichen bleibt ein leerer String übrig -> Palindrom
+        assertTrue(PalindromeChecker.isPalindrome("   "));
+    }
+
+    // B: Einfache Palindrome / Nicht-Palindrome
+
+
     @Test
     public void simpleLowercasePalindrome_returnsTrue() {
         assertTrue(PalindromeChecker.isPalindrome("anna"));
@@ -18,13 +39,41 @@ public class PalindromeCheckerTest {
     }
 
     @Test
+    public void singleCharacter_isPalindrome() {
+        assertTrue(PalindromeChecker.isPalindrome("a"));
+    }
+
+    // C: Groß-/Kleinschreibung ignorieren
+
+    @Test
     public void mixedCasePalindrome_returnsTrue() {
         assertTrue(PalindromeChecker.isPalindrome("Anna"));
+        assertTrue(PalindromeChecker.isPalindrome("aNnA"));
+        assertTrue(PalindromeChecker.isPalindrome("Abba"));
+    }
+
+    @Test
+    public void mixedCaseNonPalindrome_returnsFalse() {
+        assertFalse(PalindromeChecker.isPalindrome("AnNaX"));
+    }
+
+    // D: Leerzeichen ignorieren
+
+    @Test
+    public void palindromeWithLeadingOrTrailingSpaces_returnsTrue() {
+        assertTrue(PalindromeChecker.isPalindrome(" anna"));
+        assertTrue(PalindromeChecker.isPalindrome("anna "));
+        assertTrue(PalindromeChecker.isPalindrome("a n n a"));
     }
 
     @Test
     public void palindromeWithSpaces_returnsTrue() {
-        assertTrue(PalindromeChecker.isPalindrome("Lager Regal"));
+        assertTrue(PalindromeChecker.isPalindrome("lager regal"));
+    }
+
+    @Test
+    public void nonPalindromeEvenIfSpacesIgnored_returnsFalse() {
+        assertFalse(PalindromeChecker.isPalindrome("lager regall"));
     }
 
     @Test
@@ -32,25 +81,24 @@ public class PalindromeCheckerTest {
         assertTrue(PalindromeChecker.isPalindrome("Relief Pfeiler"));
     }
 
+
+    // E: Ziffern, Satzzeichen & Spezialzeichen
+
     @Test
-    public void emptyString_returnsTrue() {
-        assertTrue(PalindromeChecker.isPalindrome(""));
+    public void numericPalindrome_returnsTrue() {
+        assertTrue(PalindromeChecker.isPalindrome("1221"));
+        assertTrue(PalindromeChecker.isPalindrome("1 2 2 1"));
+    }
+
+
+    @Test
+    public void onlyPunctuation_returnsTrue() {
+        assertTrue(PalindromeChecker.isPalindrome(",..,..,"));
     }
 
     @Test
-    public void singleCharacter_returnsTrue() {
-        assertTrue(PalindromeChecker.isPalindrome("x"));
-    }
-
-    @Test
-    public void onlySpaces_returnsTrue() {
-        // Nach Entfernen der Leerzeichen bleibt ein leerer String übrig -> Palindrom
-        assertTrue(PalindromeChecker.isPalindrome("   "));
-    }
-
-    @Test
-    public void nullInput_returnsFalse() {
-        assertFalse(PalindromeChecker.isPalindrome(null));
+    public void specialLetters_returnsTrue() {
+        assertTrue(PalindromeChecker.isPalindrome("&%&%&"));
     }
 
 }
